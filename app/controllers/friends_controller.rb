7c0ff954de8c0bb50a8a98e5477dc17e70db9a85
Oscar Welton <oscarwelton@gmail.com>
2023-04-25 17:30:16 +0100

    first commit

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,13 +1,19 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!,  except: [:index, :show]
 
   # GET /friends or /friends.json
   def index
-    @friends = Friend.all
+    if current_user
+      @friends = Friend.where(user_id: current_user.id)
+    else
+      @friends = []
+    end
   end
 
   # GET /friends/1 or /friends/1.json
   def show
+    @friend = Friend.find(params[:id])
   end
 
   # GET /friends/new
@@ -22,6 +28,7 @@ class FriendsController < ApplicationController
   # POST /friends or /friends.json
   def create
     @friend = Friend.new(friend_params)
+    @friend.user_id = current_user.id
 
     respond_to do |format|
       if @friend.save
@@ -65,6 +72,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :socials)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :socials, :user_id)
     end
 end
